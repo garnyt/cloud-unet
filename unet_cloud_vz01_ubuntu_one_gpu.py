c0708672b95adc0dd066ece68f72f3fe576c28c4
Tania Kleynhans <garnyt@users.noreply.github.com>
2022-10-11 08:24:22 -0400

    updated patches and added comments

--- unet_cloud_vz01_ubuntu_one_gpu.py
+++ unet_cloud_vz01_ubuntu_one_gpu.py
@@ -1,67 +1,58 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 19 08:29:29 2022
-
-Based on: https://github.com/bnsreenu/python_for_image_processing_APEER/blob/master/tutorial122_3D_Unet.ipynb
-
-@author: TaniaKleynhans
-"""
+"""Unet model to predict clouds using one GPU."""
 import os
 import numpy as np
 import rasterio
 import time
 import matplotlib
-# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import tensorflow as tf
 from patchify import patchify, unpatchify
 from sklearn.model_selection import train_test_split
-#import tensorflow.keras.api._v2.keras as keras
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, UpSampling3D, concatenate, Conv3DTranspose, BatchNormalization, Dropout, Lambda
+from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, UpSampling3D, concatenate, Conv3DTranspose
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Activation, Concatenate
+from tensorflow.keras.layers import Activation, Concatenate, BatchNormalization, Dropout, Lambda
 from tensorflow.keras.utils import to_categorical
-#from keras.utils.np_utils import to_categorical
-from sklearn.model_selection import train_test_split
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import load_model
 
 from tensorflow.python.keras.losses import CategoricalCrossentropy
 from tensorflow.python.keras.metrics import CategoricalAccuracy
 
-import tensorflow as tf
-
 
 
 def conv_block(input, num_filters):
+    """Defind convolution."""
     x = Conv3D(num_filters, 3, padding="same")(input)
-    x = BatchNormalization()(x)   #Not in the original network. 
+    x = BatchNormalization()(x)
     x = Activation("relu")(x)
 
     x = Conv3D(num_filters, 3, padding="same")(x)
-    x = BatchNormalization()(x)  #Not in the original network
+    x = BatchNormalization()(x)
     x = Activation("relu")(x)
 
     return x
 
-#Encoder block: Conv block followed by maxpooling
+
 def encoder_block(input, num_filters):
+    """Encoder block: Conv block followed by maxpooling."""
     x = conv_block(input, num_filters)
     p = MaxPooling3D((2, 2, 1))(x)
-    return x, p   
+    return x, p
 
 
-#Decoder block
-#skip features gets input from encoder for concatenation
 def decoder_block(input, skip_features, num_filters):
+    """Decoder block.
+    skip features gets input from encoder for concatenation"""
     x = Conv3DTranspose(num_filters, (2, 2, 2), strides=(2,2,1), padding="same")(input)
     x = Concatenate()([x, skip_features])
     x = conv_block(x, num_filters)
     return x
 
 
-#Build Unet using the blocks
+
 def build_unet(input_shape, n_classes):
+    """Build Unet using the blocks."""
     inputs = Input(input_shape)
 
     s1, p1 = encoder_block(inputs, 64)
@@ -76,20 +67,12 @@ def build_unet(input_shape, n_classes):
     d3 = decoder_block(d2, s2, 128)
     d4 = decoder_block(d3, s1, 64)
 
-    # s1, p1 = encoder_block(inputs, 64)
-    # s2, p2 = encoder_block(p1, 128)
-
-    # b1 = conv_block(p2, 256) #Bridge
-
-    # d3 = decoder_block(b1, s2, 128)
-    # d4 = decoder_block(d3, s1, 64)
-
     if n_classes == 1:  #Binary
       activation = 'sigmoid'
     else:
       activation = 'softmax'
-
-    outputs = Conv3D(n_classes, 1, padding="same", activation=activation)(d4)  #Change the activation based on n_classes
+    # Change the activation based on n_classes
+    outputs = Conv3D(n_classes, 1, padding="same", activation=activation)(d4)
     print(activation)
 
     model = Model(inputs, outputs, name="U-Net")
@@ -150,11 +133,11 @@ def load_training_data(filename):
     rgb[:,:,2] = B/np.nanmax(B)
     rgb = rgb
     
-    return data, label, qmask, c1bqa, rgb    
+    return data, label, qmask, c1bqa, rgb
 
 
 def apptemp(img,  band ='b10'):
-    # K1 and K2 constants to be found in MTL.txt file for each band
+    """Convert toa radiance to apparent temperature."""
     if band == 'b10':
         K2 = 1321.0789;
         K1 = 774.8853;
@@ -170,6 +153,7 @@ def apptemp(img,  band ='b10'):
 
 
 def load_and_format_training_data(filepath):
+    """Load and format the training data into patches."""
 
     cnt = 0
     for file in os.listdir(filepath):
@@ -187,37 +171,35 @@ def load_and_format_training_data(filepath):
                     mask = np.hstack((mask, label))
             except:
                 pass
-    
-             # (256,256,256) # (1000,79000)
-    
-    n_classes = 7 #np.unique(mask).shape[0]
-    
-    # create smaller section per image 
-    img_patches = patchify(image, (64, 64, 6), step=62)  # (4,4,4,64,64,64) (8, 698, 1, 128, 128, 6)#Step=64 for 64 patches means no overlap
-    mask_patches = patchify(mask.astype(int), (64, 64), step=62)  # (4,4,4,64,64,64) (8, 698, 128, 128)
 
-    # img_patches = patchify(image, (128, 128, 6), step=109)  # (4,4,4,64,64,64) (8, 698, 1, 128, 128, 6)#Step=64 for 64 patches means no overlap
-    # mask_patches = patchify(mask.astype(int), (128, 128), step=109)  # (4,4,4,64,64,64) (8, 698, 128, 128)
+    n_classes = 7
 
-    input_img = np.reshape(img_patches, (-1, img_patches.shape[3], img_patches.shape[4], img_patches.shape[5])) #(64,64,64,64) (5584, 128, 128, 6)
-    input_mask = np.reshape(mask_patches, (-1, mask_patches.shape[2], mask_patches.shape[3])) #(64,64,64,64) (5584, 128, 128)
+    # create smaller section per image (patches)
+    img_patches = patchify(image, (64, 64, 6), step=62)  # Step=64 for 64 patches means no overlap
+    mask_patches = patchify(mask.astype(int), (64, 64), step=62)
+
+    # img_patches = patchify(image, (128, 128, 6), step=109)
+    # mask_patches = patchify(mask.astype(int), (128, 128), step=109)
+
+    input_img = np.reshape(img_patches, (-1, img_patches.shape[3], img_patches.shape[4], img_patches.shape[5]))
+    input_mask = np.reshape(mask_patches, (-1, mask_patches.shape[2], mask_patches.shape[3]))
     
-    # convert to each channel to rgb for now...
-    train_img = np.stack((input_img,)*1, axis=-1) # (64,64,64,64,3) (5584, 128, 128, 6, 1)
+    # convert to each channel to rgb for now... this is how model works at the moment
+    train_img = np.stack((input_img,)*1, axis=-1)
     train_img = train_img / np.max(train_img) 
-    train_mask = np.stack((input_mask,)*6, axis=-1) #(5584, 128, 128, 6)
-    train_mask = np.expand_dims(train_mask, axis=4) # (64,64,64,64,1) (5584, 128, 128, 6, 1)
+    train_mask = np.stack((input_mask,)*6, axis=-1)
+    train_mask = np.expand_dims(train_mask, axis=4)
 
-    
+    # create one hot vectors
     train_mask_cat = to_categorical(train_mask, num_classes=n_classes)
+    # split test and training set - 10%
     X_train, X_test, y_train, y_test = train_test_split(train_img, train_mask_cat, test_size = 0.10, random_state = 0)
     
     return n_classes, X_train, X_test, y_train, y_test 
 
 
 def plotting_results(history):
-    ###
-    #plot the training and validation IoU and loss at each epoch
+    """Plot the training and validation loss at each epoch."""
     loss = history.history['loss']
     val_loss = history.history['val_loss']
     epochs = range(1, len(loss) + 1)
@@ -230,10 +212,10 @@ def plotting_results(history):
     plt.ylabel('Loss')
     plt.legend()
     plt.show()
-    
+
     acc = history.history['categorical_accuracy']
     val_acc = history.history['val_categorical_accuracy']
-    
+
     plt.subplot(1,2,2)
     plt.plot(epochs, acc, 'y', label='Categorical accuracy')
     plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
@@ -242,55 +224,54 @@ def plotting_results(history):
     plt.ylabel('Accuracy')
     plt.legend()
     plt.show()
-    
+
     plt.savefig('../models/history_plot.png')
-    
-    
+
+
 def main_one_gpu(model_filename, data_filepath, batch_size, epochs):
+    """Run application."""
 
     start_time = time.time()
 
     n_classes, X_train, X_test, y_train, y_test = load_and_format_training_data(data_filepath)
     
     patches, patch_size_x, patch_size_y, patch_size_z, channels = X_train.shape
-    
+
     input_shape = [patch_size_x, patch_size_y, patch_size_z, channels]
-        
-        
+
     model = build_unet(input_shape, n_classes=n_classes)
 
+    # define learning rate
     LR = 0.0001
     optim = Adam(LR)
-    
+    # Complie model
     model.compile(optimizer=optim, loss=CategoricalCrossentropy(), metrics=CategoricalAccuracy())
-    
     print(model.summary())
     print("Patches: ", patches)
     
-    #Fit the model
+    # Fit the model
     history=model.fit(X_train, 
               y_train,
               batch_size=batch_size, 
               epochs=epochs,
               verbose=1,
               validation_data=(X_test, y_test))
-    
+
     end_time = time.time()
     print('Total time in min: ',(end_time - start_time)/60)
-    
+
     # save model
     model.save(model_filename)
-    
+
     plotting_results(history)
 
-
     return model, history
-    
-    
-model_filename = '../models/sparcs_3D_100epochs_32bs_64patch_1gpu.h5'
-data_filepath = '../scenes/'
+
+
 batch_size = 32
 epochs = 100
+data_filepath = '../scenes/'
+model_filename = f'../models/sparcs_3D_{epochs}epochs_{batch_size}bs_64patch_1gpu.h5'
 
 model, history = main_one_gpu(model_filename, data_filepath, batch_size, epochs)
 
